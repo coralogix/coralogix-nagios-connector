5c6042b1856b55c25e90403bd03e987f40c83b19
Tal Knopf <talknopf@users.noreply.github.com>
2021-11-11 14:31:39 +0200

    fixed deployment script & some minor bugs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,23 +2,42 @@
 #check if we have an argument to config the bucket name
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -b|--bucket) bucket_name="$2"; 
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+        -b|--bucket)
+            bucket_name="$2"
+            ;;
+        -h|--help)
+        echo "Usage:"; 
+        echo "${0} --bucket \"<your S3 bucket name here>\" "
+        exit 0
+            ;;
     esac
     shift
 done
 
 [[ ${bucket_name} ]] || exit 1
- 
+
 sudo apt-get update
 sudo apt install python3-pip -y
-cd /home/ubuntu/ &&
-cd nagios-alerts-connector
-#replace the bucket name in the service config files
-sed -i "s/tbd/${bucket_name}/" /home/ubuntu/nagios-alerts-connector/nagios-nest.config
 
-pip3 install -r ./requirements.txt
-sudo cp /home/ubuntu/nagios-alerts-connector/service_files/nagios-nest.service /etc/systemd/system/
+#install python dependencies
+cd ..
+pip3 install -r ./requirements.txt &&
+
+current_folder=$(pwd)
+
+#replace the bucket name in the service config files
+sed -i "s|tbd|${bucket_name}|" ${current_folder}/service_files/nagios-nest.config
+
+#replace working dir in service file
+sed -i "s|dir1|${current_folder}|" ${current_folder}/service_files/nagios-nest.service
+
+#replace config location in service file
+sed -i "s|dir2|${current_folder}|" ${current_folder}/service_files/nagios-nest.service
+
+#place the service file in it's target folder
+sudo cp ${current_folder}/service_files/nagios-nest.service /etc/systemd/system/
+
+#reload the demon and start the service
 sudo systemctl daemon-reload
 sudo systemctl start nagios-nest.service
 sudo systemctl enable nagios-nest.service
